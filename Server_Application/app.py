02939b107ba95c693304dd1cdc333b355d7f722f
shannentan22 <astan9@up.edu.ph>
2023-05-18 18:32:43 +0800

    Added sensor tables

--- Server_Application/app.py
+++ Server_Application/app.py
@@ -2,17 +2,40 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-data = 'initial data'  # Initial data for display
+devices = [
+    {
+        'Timestamps': ["0", "1"], 
+        'WaterLevels': ["Low", "Moderate"],
+    }
+]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global data  # Use the global data variable
 
     if request.method == 'POST':
-        new_data = request.form['data']
-        data = new_data  # Update the data with the new value
+        new_data = request.get_json()
+        # Input format: { 'DeviceID': '', 'Timestamp': '', 'WaterLevel': ''}
+        if new_data:
+            DeviceID = int(new_data.get('DeviceID'))
+            Timestamp = new_data.get('Timestamp')
+            WaterLevel = new_data.get('WaterLevel')
+            update(DeviceID, Timestamp, WaterLevel)
+        
 
-    return render_template('index.html', data=data)
+    return render_template('index.html', devices=devices)
+
+def update(DeviceID, Timestamp, WaterLevel):
+    if DeviceID < len(devices):
+        devices[DeviceID]['Timestamps'].append(Timestamp)
+        devices[DeviceID]['WaterLevels'].append(WaterLevel)
+        return
+    else:
+        new_device = {
+            'Timestamps': [Timestamp],
+            'WaterLevels': [WaterLevel],
+        }
+        devices.append(new_device)
 
 if __name__ == '__main__':
     app.run()
