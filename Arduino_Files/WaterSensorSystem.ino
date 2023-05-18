ff4febc868bf86b5d4bbaa0d5571363e13feab86
Ray Nuñez <75060558+nunezrrr@users.noreply.github.com>
2023-05-18 20:57:15 +0800

    Readability Adjustments

--- Arduino_Files/WaterSensorSystem.ino
+++ Arduino_Files/WaterSensorSystem.ino
@@ -1,40 +1,47 @@
 // This File is used to detect the water level within a singular sensor
 // This will be used in case we would want to change "Trigger" Values in the actual system
 
-
 #define POWER_PIN  7
 
-int value1 = 0; // variable to store WaterSensor1's value
-int value2 = 0; // variable to store WaterSensor1's value
-int value3 = 0; // variable to store WaterSensor1's value
+#define MODERATESENSOR A5
+#define HIGHSENSOR A6
+#define VERYHIGHSENSOR A7
+
+#define THRESHOLD 100
+
+int moderate_sensor_value = 0; // variable to store WaterSensor1's value
+int high_sensor_value = 0; // variable to store WaterSensor1's value
+int veryhigh_sensor_value = 0; // variable to store WaterSensor1's value
+
+
 
 int level = 0; // variable to store the water level
-int WaterSensor1 = A5;
-int WaterSensor2 = A6;
-int WaterSensor3 = A7; 
-
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-    value1 = analogRead(WaterSensor1);
-    value2 = analogRead(WaterSensor2);
-    value3 = analogRead(WaterSensor3);
+    //moderate_sensor_value = analogRead(MODERATESENSOR);
+    //high_sensor_value = analogRead(HIGHSENSOR);
+    //veryhigh_sensor_value = analogRead(VERYHIGHSENSOR);
 
-    if (value1 <= 100){
-        Serial.println("Water Level: Empty");
+    moderate_sensor_value = 100;
+    high_sensor_value = 200;
+    veryhigh_sensor_value = 100;
+
+    if (moderate_sensor_value < THRESHOLD){
+        Serial.println("Water Level: Low");
     }
     else {
-        if (value1 >= 100 && value2 <= 100 && value3 <= 100){
-            Serial.println("Water Level: Low")
+        if (moderate_sensor_value >= THRESHOLD && high_sensor_value < THRESHOLD && veryhigh_sensor_value < THRESHOLD){
+            Serial.println("Water Level: Moderate");
         }
-        else if (value1 >= 100 && value2 >= 100 && value3 <= 100){
-            Serial.println("Water Level: Medium")
+        else if (moderate_sensor_value >= THRESHOLD && high_sensor_value >= THRESHOLD && veryhigh_sensor_value < THRESHOLD){
+            Serial.println("Water Level: High");
         }
-        else if (value1 >= 100 && value2 >= 100 && value3 >= 100){
-            Serial.println("Water Level: High")
+        else if (moderate_sensor_value >= THRESHOLD && high_sensor_value >= THRESHOLD && veryhigh_sensor_value >= THRESHOLD){
+            Serial.println("Water Level: Very High");
         }
     }
     delay(1000);
